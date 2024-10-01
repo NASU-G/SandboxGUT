64f4eae0b6936382991cf02046fbe5cd7a843d76
NASU-G <nasu.g.game@gmail.com>
2024-10-01 16:31:34 +0900

    Global を追加した。

---
+++ scenes/global.gd
@@ -0,0 +1,27 @@
+#class_name Global	# 不要	# Global.AAA で、AAA にアクセス可能
+extends Node
+
+# @GlobalScope enum Error # https://docs.godotengine.org/ja/4.x/classes/class_@globalscope.html#enum-globalscope-error
+var error: Error = Error.FAILED
+
+const MAIN_SCENE_PATH = "res://scenes/main_scene/main_scene.tscn"
+
+const MY_SLIDER_DEFAULT_VALUE = 0.2	# MyHSlider の初期値
+var my_slider_value: float = MY_SLIDER_DEFAULT_VALUE	# MyHSlider の現在値
+
+
+func _ready() -> void:
+	cold_reset()
+	warm_reset()
+
+
+# 動作初回と、テストのときに呼ぶことを想定した、より厳しいリセット処理
+func cold_reset() -> void:
+	my_slider_value = MY_SLIDER_DEFAULT_VALUE
+	return
+
+
+# 各sceneの_ready()で呼ぶことを想定した、リセット処理
+func warm_reset() -> void:
+	error = FAILED
+	return
